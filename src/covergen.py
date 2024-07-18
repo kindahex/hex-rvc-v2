67df0e3f970d9c754abeb5284fc0730bd8d4295d
Artyom Bebroy <Politrees@users.noreply.huggingface.co>
2024-07-18 13:45:04 +0000

    Update src/covergen.py

--- src/covergen.py
+++ src/covergen.py
@@ -20,10 +20,12 @@ if __name__ == '__main__':
     voice_models = ignore_files(rvc_models_dir)
 
     with gr.Blocks(title='CoverGen Lite - Politrees (v0.2)', theme=gr.themes.Soft(primary_hue="green", secondary_hue="green", neutral_hue="neutral", spacing_size="sm", radius_size="lg")) as app:
+        
+        with gr.Column(variant='panel'):
+            gr.HTML("<center><h2>This space is running too slow due to a weak server, so I made a Google Colab notebook to work faster with this interface: <a href='https://colab.research.google.com/drive/1HzuPgICRrjqUWQWb5Zn-l07m099-n-Nr'>Google Colab Notebook</a>.</h2></center>")
+        
         with gr.Tab("Welcome/Contacts"):
             gr.HTML("<center><h1>Welcome to CoverGen Lite - Politrees (v0.2)</h1></center>")
-            with gr.Column(variant='panel'):
-                gr.HTML("<center><h2>This space is running too slow due to a weak server, so I made a Google Colab notebook to work faster with this interface: <a href='https://colab.research.google.com/drive/1HzuPgICRrjqUWQWb5Zn-l07m099-n-Nr'>Google Colab Notebook</a>.</h2></center>")
             with gr.Row():
                 with gr.Column(variant='panel'):
                     gr.HTML("<center><h2><a href='https://t.me/Politrees2'>Telegram</a></h2></center>")
