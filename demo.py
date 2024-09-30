5188ac75c79240ace527db47618a4dc816c71413
Rico Ardiansyah <167639579+blane187gt@users.noreply.github.com>
2024-10-01 00:36:52 +0700

    Update demo.py

--- demo.py
+++ demo.py
@@ -15,8 +15,7 @@ from modules.ui_updates import show_hop_slider, update_f0_method, update_button_
 from modules.file_processing import process_file_upload
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+BASE_DIR = "/content/hex"
 
 
 
