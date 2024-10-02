a2002f1740cf66da85fa1c2471d0269a10031e46
Rico Ardiansyah <167639579+blane187gt@users.noreply.github.com>
2024-10-02 07:59:14 +0700

    Update download_models.py

--- download_models.py
+++ download_models.py
@@ -4,11 +4,12 @@ import requests
 RVC_DOWNLOAD_LINK = 'https://huggingface.co/kindahex/voice-conversion/resolve/main/'
 
 BASE_DIR = "/content/hex"
-rvc_models_dir = f'{BASE_DIR}/rvc_models'
+rvc_models_dir = Path(f'{BASE_DIR}/rvc_models')
 
 
 def dl_model(link, model_name, dir_name):
-    with requests.get(f'{link}{model_name}') as r:
+    dir_name.mkdir(parents=True, exist_ok=True)  # Ensure directory exists
+    with requests.get(f'{link}{model_name}', stream=True) as r:
         r.raise_for_status()
         with open(dir_name / model_name, 'wb') as f:
             for chunk in r.iter_content(chunk_size=8192):
@@ -19,6 +20,5 @@ if __name__ == '__main__':
     for model in rvc_other_names:
         print(f'Downloading {model}...')
         dl_model(RVC_DOWNLOAD_LINK, model, rvc_models_dir)
+        print(f'{model} downloaded successfully!')
 
-
-    print(f'A {model} downloaded!')
